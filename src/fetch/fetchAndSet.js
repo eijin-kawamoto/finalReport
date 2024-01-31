4360dfe25656cd5becb500b46986e5f48af1d9cf
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-01 00:12:51 +0900

    fetchAndSet.jsの変更

--- src/fetch/fetchAndSet.js
+++ src/fetch/fetchAndSet.js
@@ -4,8 +4,15 @@ export const fetchAndSet = async (url, setFunction) => {
       if(!response.ok) {
         throw new Error(`Failed to fetch data from ${url}`);
       }
-      const data = await response.json();
-      setFunction(data.message);
+
+      const contentType = response.headers.get("content-type");
+
+      if (contentType && contentType.includes("application/json")) {
+        const data = await response.json();
+        setFunction(data.message);
+      } else {
+        throw new Error(`Invalid response format form ${url}`);
+      }
     } catch (error) {
       console.error("Error fetching data:", error);
     }
