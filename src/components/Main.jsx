749c2549bbe5ee99e0746fc4605ee374bf8b6fac
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-01-31 23:56:04 +0900

    CORSの対応

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,13 +12,13 @@ export default function Main() {
     const fetchData = async(query) => {
       try {
         if(query) {
-          const dogTypeData = await fetchAndSet(`dogtype/${query}`);
+          const dogTypeData = await fetchAndSet(`/api/breed/${query}/images/random`);
           setDogType(dogTypeData);
           setSearchedDogType(query);
         } else {
-          const dogData = await fetchAndSet("dog");
+          const dogData = await fetchAndSet('/api/breed/dog/images/random');
           setDog(dogData);
-          await fetchAndSet("dogtype", setDogType);
+          await fetchAndSet('/api/breed/dogtype/images/random', setDogType);
           setSearchedDogType("");
         }
       } catch(error) {
