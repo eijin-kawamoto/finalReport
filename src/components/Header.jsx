34eb837267bf93471fa489e752fb764fce0fa46b
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-01 23:51:13 +0900

    ヘッダーの改良

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Container } from "@mui/material";
 
 export default function Header() {
-    return (
-      <header className="header" 
-      style={{textAlign: "center", padding: "20px"}}>
-        <Typography variant="h1" style={{ margin: 0, color: "#333" }}>
-          犬
-        </Typography>
-      </header>
-    );
+  return (
+    <AppBar position="static">
+      <Container maxWidth="md">
+        <Toolbar>
+          <img
+            src="/dog_icon.png"
+            alt="Dog Icon"
+            style={{ marginRight: "10px", width: "30px", height: "30px" }}
+          />
+          <Typography variant="h1">犬</Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
