1d0a04297d276ebffc953c1cc2b6f434f48efaaf
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-01 23:26:15 +0900

    ボタンの追加

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -36,7 +36,7 @@ export default function Main() {
         <Typography variant="h4" gutterBottom>
           Random Dog Images
         </Typography>
-        <Button variant="contained" color="#f0e68c" onClick={getDogImages}>
+        <Button variant="contained" color="primary" onClick={getDogImages}>
           違う画像にする
         </Button>
         <Grid container spacing={2}>
