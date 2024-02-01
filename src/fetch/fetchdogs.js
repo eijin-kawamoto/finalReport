6efb71175dd98a1a45b6b6280825dd9d307c28b2
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-02 00:08:28 +0900

    修正

--- src/fetch/fetchdogs.js
+++
@@ -1,5 +0,0 @@
-export const fetchDogs = async (type) => {
-    const response = await fetch(`https://dog.ceo/api/breed/${type}/images/random/`);
-    const data = await response.json();
-    return data;
-  };
