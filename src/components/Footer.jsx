7117b93db6f2272d2dd41f326cc681a25d2b352a
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-02 00:24:40 +0900

    フッターの背景色変更

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,15 @@ import { AppBar, Toolbar, Typography, Container } from "@mui/material";
 
 export default function Footer() {
     return (
-      <footer className="footer" 
-      style={{textAlign: "center", padding: "20px"}}>
-        <Typography variant="h5" style={{ margin: 0, color: "#333" }}>
-         日本大学文理学部情報科学科Webプログラミングの演習課題 
-        </Typography>
-        <Typography variant="h5" style={{ margin: 0, color: "#333" }}>
-         5422057 川元瑛仁
-        </Typography>
-      </footer>
+      <AppBar position="static" className="footer" style={{ backgroundColor: "gray"}}>
+        <Toolbar style={{ justifyContent: "center" }}>
+          <Typography variant="h5" style={{ margin: 0, color: "#333" }}>
+            日本大学文理学部情報科学科Webプログラミングの演習課題 
+          </Typography>
+          <Typography variant="h5" style={{ margin: 0, color: "#333" }}>
+            5422057 川元瑛仁
+          </Typography>
+        </Toolbar>
+      </AppBar>
     );
 }
