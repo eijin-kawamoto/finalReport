1db8594d9aa45a473ec72e2559c6ade16d31e6a3
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-02 00:17:01 +0900

    ヘッダーの背景色変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,12 @@ import { AppBar, Toolbar, Typography, Container } from "@mui/material";
 
 export default function Header() {
     return (
-      <header className="header" 
-      style={{textAlign: "center", padding: "20px"}}>
-        <Typography variant="h1" style={{ margin: 0, color: "#333" }}>
-          犬
-        </Typography>
-      </header>
+      <AppBar position="static" className="header" style={{ backgroundColor: "antiquewhite" }}>
+        <Toolbar>
+          <Typography variant="h1" style={{ margin: 0, color: "#333" }}>
+            犬
+          </Typography>
+        </Toolbar>
+      </AppBar>
     );
 }
