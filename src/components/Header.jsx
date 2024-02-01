49f918fa63b69c585183ccd27deecc6390689765
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-02 00:20:02 +0900

    ヘッダーの改良

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { AppBar, Toolbar, Typography, Container } from "@mui/material";
 export default function Header() {
     return (
       <AppBar position="static" className="header" style={{ backgroundColor: "antiquewhite" }}>
-        <Toolbar>
+        <Toolbar style={{ justifyContent: "center" }}>
           <Typography variant="h1" style={{ margin: 0, color: "#333" }}>
             犬
           </Typography>
