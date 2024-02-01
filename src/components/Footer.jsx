3c017acd352883707f6b0c47ca51e94ae5c139be
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-02 00:13:09 +0900

    フッターの改良

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,16 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Container } from "@mui/material";
+
 export default function Footer() {
     return (
-      <footer className="footer">
-        <p>日本大学文理学部情報科学科Webプログラミングの演習課題</p>
-        <p>5422057 川元瑛仁</p>
+      <footer className="footer" 
+      style={{textAlign: "center", padding: "20px"}}>
+        <Typography variant="h5" style={{ margin: 0, color: "#333" }}>
+         日本大学文理学部情報科学科Webプログラミングの演習課題 
+        </Typography>
+        <Typography variant="h5" style={{ margin: 0, color: "#333" }}>
+         5422057 川元瑛仁
+        </Typography>
       </footer>
     );
 }
