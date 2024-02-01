e134623b1f1c7c001fc2c25971f996686be645b3
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-01 21:54:29 +0900

    Main.jsxの変更

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,39 +2,46 @@ import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 
 export default function Main() {
-  const [dogImageUrl, setDogImageUrl] = useState("");
+  const dogBreeds = ["akita", "beagle", "bulldog", "chihuahua", "husky", "poodle", "shiba"];
+  const [dogImages, setDogImages] = useState({});
 
   useEffect(() => {
-    const getDogImage = async () => {
+    const fetchDogImages = async (breed) => {
       try {
-        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const response = await fetch("https://dog.ceo/api/breed/${breed}/images/random");
 
         const data = await response.json();
 
-        setDogImageUrl(data.message);
+        setDogImages((prevDogImages) => ({
+            ...prevDogImages,
+            [breed]: data.message,
+        }));
       } catch (error) {
-        console.error("Error fetching dog image:", error);
+        console.error("Error fetching ${breed} image:", error);
       }
     };
 
-    getDogImage();
+    dogBreeds.forEach((breed) => fetchDogImages(breed));
   }, []);
 
   return (
     <main>
       <section className="section">
         <Typography variant="h4" gutterBottom>
-          Random Dog Image
+          Dog Breed Images
         </Typography>
-        {dogImageUrl && (
-          <Box>
-            <img
-              src={dogImageUrl}
-              alt="Random Dog"
-              style={{ maxWidth: "100%", height: "auto" }}
-            />
-          </Box>
-        )}
+        {dogBreeds.map((breed) => (
+            <Box key={breed} mb={2}>
+                <Typography variant="h6">{breed}</Typography>
+                {dogImages[breed] && (
+                  <img
+                    src={dogImages[breed]}
+                    alt={`${breed} Dog`}
+                    style={{ maxWidth: "100%", height: "auto" }}
+                  />
+                )}
+              </Box>
+            ))}
       </section>
     </main>
   );
