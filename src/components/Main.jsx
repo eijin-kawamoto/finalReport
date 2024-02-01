9cf62beea6329d7987903b8170ba668f4ba2f3a9
eijin-kawamoto <chei22057@g.nihon-u.ac.jp>
2024-02-01 22:19:24 +0900

    Mainの変更

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,40 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 
+const dogBreeds = ["akita", "beagle", "frenchbulldog", "chihuahua", "husky"];
+
 export default function Main() {
-  const [dogImageUrl, setDogImageUrl] = useState("");
+  const [dogImages, setDogImages] = useState([]);
 
   useEffect(() => {
-    const getDogImage = async () => {
+    const getDogImages = async () => {
       try {
-        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const dogImagesPromises = dogBreeds.map(async (breed) => {
+          const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+          const data = await response.json();
+          return { breed, imageUrl: data.message };
+        });
 
-        const data = await response.json();
-
-        setDogImageUrl(data.message);
+        const resolvedDogImages = await Promise.all(dogImagesPromises);
+        setDogImages(resolvedDogImages);
       } catch (error) {
-        console.error("Error fetching dog image:", error);
+        console.error("Error fetching dog images:", error);
       }
     };
 
-    getDogImage();
+    getDogImages();
   }, []);
 
   return (
     <main>
       <section className="section">
         <Typography variant="h4" gutterBottom>
-          Random Dog Image
+          Random Dog Images
         </Typography>
-        {dogImageUrl && (
-          <Box>
+        {dogImages.map((dogImage, index) => (
+          <Box key={index}>
             <img
-              src={dogImageUrl}
-              alt="Random Dog"
+              src={dogImage.imageUrl}
+              alt={`Random ${dogImage.breed}`}
               style={{ maxWidth: "100%", height: "auto" }}
             />
+            <Typography variant="caption">{dogImage.breed}</Typography>
           </Box>
-        )}
+        ))}
       </section>
     </main>
   );
